a3e3e8805a9b2992ad6f820704c5813f1125a803
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2021-12-03 14:51:27 +0100

    Create DimmableGrid

---
+++ src/test/java/DimmableGridTest.java
@@ -0,0 +1,19 @@
+import com.titusnachbauer.christmaslights.DimmableGrid;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DimmableGridTest {
+
+    @Test
+    void GivenADimmableGridWhenInitializedTotalBrightnessShouldBeZero() {
+        DimmableGrid grid = new DimmableGrid();
+        Assertions.assertEquals(0, grid.calculateTotalBrightness());
+    }
+
+    @Test
+    void GivenADimmableChristmansGridWhenAllLightsAreTurnedOnTotalBrightnessShouldBe1Mln() {
+        DimmableGrid grid = new DimmableGrid(1000, 1000);
+        grid.turnOn(0, 999999);
+        Assertions.assertEquals(1000000, grid.calculateTotalBrightness());
+    }
+}
