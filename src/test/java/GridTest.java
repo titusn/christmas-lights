cd715f70d128e6bb3171914e37cc08d2150e7e91
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2021-12-03 14:24:30 +0100

    Reformat

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -7,51 +7,51 @@ class GridTest {
     Grid christmasGrid = new Grid(1000, 1000);
 
     @Test
-    void GivenAGridWhenInitializedNoLightsShouldBeOn () {
+    void GivenAGridWhenInitializedNoLightsShouldBeOn() {
         Grid grid = new Grid();
-        assertEquals(0 , grid.countLightsOn());
+        assertEquals(0, grid.countLightsOn());
     }
 
     @Test
-    void GivenATenByTenGridWhenInitializedItShouldContain100Lights () {
+    void GivenATenByTenGridWhenInitializedItShouldContain100Lights() {
         Grid grid = new Grid(10, 10);
         assertEquals(100, grid.countLights());
     }
 
     @Test
-    void GivenAChristmasGridWhenInitializedItShouldContainAMillionLights () {
+    void GivenAChristmasGridWhenInitializedItShouldContainAMillionLights() {
         assertEquals(1000000, christmasGrid.countLights());
     }
 
     @Test
-    void GivenAChristmasGridWhenAllCoordinatesAreTurnedOnAllLightsShouldBeOn () {
-        christmasGrid.turnOn(0,999999);
+    void GivenAChristmasGridWhenAllCoordinatesAreTurnedOnAllLightsShouldBeOn() {
+        christmasGrid.turnOn(0, 999999);
         assertEquals(1000000, christmasGrid.countLightsOn());
     }
 
     @Test
-    void GivenAChristmasGridWhen1CoordinateIsTurnedOn1LightShouldBeOn () {
-        christmasGrid.turnOn(0,0);
+    void GivenAChristmasGridWhen1CoordinateIsTurnedOn1LightShouldBeOn() {
+        christmasGrid.turnOn(0, 0);
         assertEquals(1, christmasGrid.countLightsOn());
     }
 
     @Test
-    void GivenAChristmasGridWhen10CoordinatesAreTurnedOffAfterTurningAllOn999990ShouldBeOn () {
-        christmasGrid.turnOn(0,999999);
-        christmasGrid.turnOff(0,9);
+    void GivenAChristmasGridWhen10CoordinatesAreTurnedOffAfterTurningAllOn999990ShouldBeOn() {
+        christmasGrid.turnOn(0, 999999);
+        christmasGrid.turnOff(0, 9);
         assertEquals(999990, christmasGrid.countLightsOn());
     }
 
     @Test
-    void GivenAChristmasGridWhen111CoordinatesAreToggled111LightsShouldBeOn () {
-        christmasGrid.toggle(0,110);
+    void GivenAChristmasGridWhen111CoordinatesAreToggled111LightsShouldBeOn() {
+        christmasGrid.toggle(0, 110);
         assertEquals(111, christmasGrid.countLightsOn());
     }
 
     @Test
-    void GivenAChristmasGridWhen10CoordinatesAreToggledAfter100AreTurnedOn90LightsShouldBeOn () {
-        christmasGrid.turnOn(200,299);
-        christmasGrid.toggle(230,239);
+    void GivenAChristmasGridWhen10CoordinatesAreToggledAfter100AreTurnedOn90LightsShouldBeOn() {
+        christmasGrid.turnOn(200, 299);
+        christmasGrid.toggle(230, 239);
         assertEquals(90, christmasGrid.countLightsOn());
     }
 
