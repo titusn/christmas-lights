51c1038ea0264cba061f901b9d5ccdb72bd427f0
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2021-12-03 12:22:02 +0100

    Refactor imports

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -1,25 +1,26 @@
 import com.titusnachbauer.christmaslights.Grid;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GridTest {
 
     @Test
     void GivenAGridWhenInitializedNoLightsShouldBeOn () {
         Grid grid = new Grid();
-        Assertions.assertEquals(0 , grid.countLightsOn());
+        assertEquals(0 , grid.countLightsOn());
     }
 
     @Test
     void GivenATenByTenGridItShouldContain100Lights () {
         Grid grid = new Grid(10, 10);
-        Assertions.assertEquals(100, grid.countLights());
+        assertEquals(100, grid.countLights());
     }
 
     @Test
     void GivenAThousandByThousandGridItShouldContainAMillionLights () {
         Grid grid = new Grid(1000, 1000);
-        Assertions.assertEquals(1000000, grid.countLights());
+        assertEquals(1000000, grid.countLights());
     }
 
 }
