b6b094ade7717233ade778c3f5719c6793dea705
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2021-12-06 16:50:50 +0100

    Refactor

--- src/test/java/LightTest.java
+++ src/test/java/LightTest.java
@@ -1,45 +1,46 @@
 import com.titusnachbauer.christmaslights.Light;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LightTest {
     Light light = new Light();
 
     @Test
     void GivenLightWhenInitializedItIsOff() {
-        Assertions.assertTrue(light.isOff());
+        assertTrue(light.isOff());
     }
 
     @Test
     void GivenLightWhenTurnedOnItIsOn() {
         light.turnOn();
-        Assertions.assertTrue(light.isOn());
+        assertTrue(light.isOn());
     }
 
     @Test
     void GivenTurnedOnLightWhenTurnedOffItIsOff() {
         light.turnOn();
         light.turnOff();
-        Assertions.assertTrue(light.isOff());
+        assertTrue(light.isOff());
     }
 
     @Test
     void GivenTurnedOnLightWhenTurnedOnItIsOn() {
         light.turnOn();
         light.turnOn();
-        Assertions.assertTrue(light.isOn());
+        assertTrue(light.isOn());
     }
 
     @Test
     void GivenTurnedOffLightWhenToggledItIsOn() {
         light.toggle();
-        Assertions.assertTrue(light.isOn());
+        assertTrue(light.isOn());
     }
 
     @Test
     void GivenTurnedOnLightWhenToggledItIsOff() {
         light.turnOn();
         light.toggle();
-        Assertions.assertTrue(light.isOff());
+        assertTrue(light.isOff());
     }
 }
