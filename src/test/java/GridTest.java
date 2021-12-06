fa5b160b8a735684b24dc2ce69a9989473b1d715
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2021-12-06 16:01:24 +0100

    Refactor

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -1,6 +1,7 @@
 import com.titusnachbauer.christmaslights.Grid;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GridTest {
     private final Grid christmasGrid = new Grid(1000, 1000);
@@ -8,31 +9,31 @@ class GridTest {
     @Test
     void GivenGridWhenInitializedItHasZeroLights() {
         Grid grid = new Grid();
-        Assertions.assertEquals(0, grid.count());
+        assertEquals(0, grid.count());
     }
 
     @Test
     void GivenChristmasGridWhenInitializedItHas1MlnLights() {
-        Assertions.assertEquals(1000000, christmasGrid.count());
+        assertEquals(1000000, christmasGrid.count());
     }
 
     @Test
-    void GivenChristmasGridWhenLightSwitchedOnItHasOneLightOn() {
+    void GivenChristmasGridWhenLightTurnedOnItHasOneLightOn() {
         christmasGrid.turnOn(0, 0);
-        Assertions.assertEquals(1, christmasGrid.countLightsOn());
+        assertEquals(1, christmasGrid.countLightsOn());
     }
 
     @Test
-    void GivenChristmasGridWhenAllLightsSwitchedOnItHas1MlnLightsOn() {
+    void GivenChristmasGridWhenAllLightsTurnedOnItHas1MlnLightsOn() {
         christmasGrid.turnOn(0, 999999);
-        Assertions.assertEquals(1000000, christmasGrid.countLightsOn());
+        assertEquals(1000000, christmasGrid.countLightsOn());
     }
 
     @Test
-    void GivenChristmasGridWhenAllLightsTurnedOnAndOneSwitchedOffItHas999999LightsOn() {
+    void GivenChristmasGridWhenAllLightsTurnedOnAndOneTurnedOffItHas999999LightsOn() {
         christmasGrid.turnOn(0, 999999);
         christmasGrid.turnOff(999999, 999999);
-        Assertions.assertEquals(999999, christmasGrid.countLightsOn());
+        assertEquals(999999, christmasGrid.countLightsOn());
     }
 
 }
