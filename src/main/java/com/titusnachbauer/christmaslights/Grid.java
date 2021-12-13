8b06e633986b33387725e93f6d289637995948d3
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2021-12-13 14:49:01 +0100

    Rename Lights array

--- src/main/java/com/titusnachbauer/christmaslights/Grid.java
+++ src/main/java/com/titusnachbauer/christmaslights/Grid.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Grid {
-    private final Light[] newLights;
+    private final Light[] lights;
 
     public Grid(int x, int y) {
-        newLights = new Light[x * y];
-        Arrays.setAll(newLights, light -> new Light());
+        lights = new Light[x * y];
+        Arrays.setAll(lights, light -> new Light());
     }
 
     public Grid() {
@@ -16,28 +16,28 @@ public class Grid {
     }
 
     public int countLightsOn() {
-        return (int) Arrays.stream(newLights).filter(Light::isOn).count();
+        return (int) Arrays.stream(lights).filter(Light::isOn).count();
     }
 
     public int count() {
-        return newLights.length;
+        return lights.length;
     }
 
     public void turnOn(int start, int end) {
         IntStream
                 .rangeClosed(start, end)
-                .forEach(i -> newLights[i].turnOn());
+                .forEach(i -> lights[i].turnOn());
     }
 
     public void turnOff(int start, int end) {
         IntStream
                 .rangeClosed(start, end)
-                .forEach(i -> newLights[i].turnOff());
+                .forEach(i -> lights[i].turnOff());
     }
 
     public void toggle(int start, int end) {
         IntStream
                 .rangeClosed(start, end)
-                .forEach(i -> newLights[i].toggle());
+                .forEach(i -> lights[i].toggle());
     }
 }
