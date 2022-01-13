6992e161b93520a33a8cf78141cfcb55ea8f697b
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2022-01-13 11:45:32 +0100

    Add Coordinate class

--- src/main/java/com/titusnachbauer/christmaslights/Grid.java
+++ src/main/java/com/titusnachbauer/christmaslights/Grid.java
@@ -1,65 +1,28 @@
 package com.titusnachbauer.christmaslights;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class Grid {
-    private final Light[][] newLights;
+    private final int width;
+    private final int height;
+    private int lightsOn;
 
     public Grid(int x, int y) {
-        newLights = new Light[x][y];
-        IntStream.range(0, x).forEach(currentX -> Arrays.setAll(newLights[currentX], currentY -> new Light()));
+        this.width = x;
+        this.height = y;
     }
 
     public Grid() {
         this(0, 0);
     }
 
-    public Grid(Coordinate coordinate) {
-        this(coordinate.x(), coordinate.y());
+    public int count() {
+        return width * height;
     }
 
     public int countLightsOn() {
-        int count = 0;
-        for (Light[] lightRow : newLights) {
-            for (Light light : lightRow) {
-                if (light.isOn()) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    public int count() {
-        if (newLights.length > 0) {
-            return newLights.length * newLights[0].length;
-        } else {
-            return 0;
-        }
+        return lightsOn;
     }
 
     public void turnOn(Coordinate start, Coordinate end) {
-        for (int y = start.y(); y <= end.y(); y++) {
-            for (int x = start.x(); x <= end.x(); x++) {
-                newLights[x][y].turnOn();
-            }
-        }
-    }
-
-    public void turnOff(Coordinate start, Coordinate end) {
-        for (int y = start.y(); y <= end.y(); y++) {
-            for (int x = start.x(); x <= end.x(); x++) {
-                newLights[x][y].turnOff();
-            }
-        }
-    }
-
-    public void toggle(Coordinate start, Coordinate end) {
-        for (int y = start.y(); y <= end.y(); y++) {
-            for (int x = start.x(); x <= end.x(); x++) {
-                newLights[x][y].toggle();
-            }
-        }
+        lightsOn = (end.x() + 1 - start.x()) * (end.y() + 1 - start.y());
     }
 }
